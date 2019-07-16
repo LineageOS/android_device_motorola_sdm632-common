8a78e2432f9e41d3c5e10f7a43667e4407594ac2
Sujin Panicker <spanic@codeaurora.org>
2019-07-16 18:26:34 +0530

    sdm632-common: media: Fix rank of QC OMX components
    Rank OMX SW codecs higher than C2 codecs except for opus codec.
    
    Change-Id: Ided16f454fcb3b760076500e87759bcbb44f75ed

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -78,6 +78,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.fm.use_audio_session=true
 
+# OMX
+# Rank OMX SW codecs lower than OMX HW codecs
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0
+
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
