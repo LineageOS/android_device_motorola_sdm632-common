a50f2f462e656dcb04369f42f49b56a236b5db2a
Bruno Martins <bgcngm@gmail.com>
2019-09-30 13:32:04 +0100

    sdm632-common: Drop mediaextractor seccomp policy
    These syscalls are now allowed by default.
    
    Change-Id: I405f609d030331e19031612cbd912a54a3c72339
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- common.mk
+++ common.mk
@@ -387,7 +387,6 @@ PRODUCT_PACKAGES += \
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
