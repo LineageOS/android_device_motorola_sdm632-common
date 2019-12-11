f65d254a7c9e5200ba5d54a4a00cd4f477f1d10c
Erfan Abdi <erfangplus@gmail.com>
2019-12-11 08:16:42 +0330

    sdm632-common: Set moto iccid prop from utag
    Co-authored-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Icaad31e8753272fd1f64570b31d68db3a1d30db2

--- rootdir/bin/init.class_main.sh
+++ rootdir/bin/init.class_main.sh
@@ -168,3 +168,15 @@ case "$fake_batt_capacity" in
     echo "$fake_batt_capacity" > /sys/class/power_supply/battery/capacity
     ;;
 esac
+
+# reload UTAGS
+utag_status=$(cat /proc/config/reload)
+if [ "$utag_status" == "2" ]; then
+    echo 1 > /proc/config/reload
+fi
+
+# Export these for factory validation purposes
+iccid=$(cat /proc/config/iccid/ascii 2>/dev/null)
+if [ ! -z "$iccid" ]; then
+    setprop ro.vendor.mot.iccid $iccid
+fi
