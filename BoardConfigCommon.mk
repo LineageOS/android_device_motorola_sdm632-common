a018a7218ca2647313f18c5ea89284607404cff6
Vinay Verma <vvinay@codeaurora.org>
2019-12-20 15:31:05 -0800

    sdm632-common: Remove firmware class path for kernel firmware loading
    In ueventd support has been added to read firmware path from
    vendor.It wil parse /vendor/firmware_mnt/image information
    and add in look up path/search path for loading firmware.
    Change uid of /venodr/firmware_mnt back to system as kernel
    firmware loading is removed. Previouly to support kernel firmware
    loading root permissions were required.
    
    Change-Id: I560ed75ee07626094acbc879b615190cf3a9faa3
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -175,7 +175,6 @@ TARGET_FS_CONFIG_GEN += \
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci androidboot.usbconfigfs=true
-BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
