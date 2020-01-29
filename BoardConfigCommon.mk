244a9f763974ce76c8d247b724d8123d47daceca
Erfan Abdi <erfangplus@gmail.com>
2020-01-29 13:19:16 +0330

    sdm632-common: Set verity to EIO mode
    Change-Id: I64f12657ce0d0b7a8e364c5e5b1da26177dac184

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,6 +172,7 @@ TARGET_FS_CONFIG_GEN += \
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci androidboot.usbconfigfs=true
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.veritymode=eio
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE :=  2048
