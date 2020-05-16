a7b47455ef6b18c71a6766f87c2cfd6f2dc2d6f7
LuK1337 <priv.luk@gmail.com>
2020-05-16 20:28:31 +0200

    sdm632-common: Build 64-bit variants of android.hardware.camera.{device,provider}
    * Fixes: ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.provider@2.4.so" not found
    
    Change-Id: I938dd7135a53e9e0951c31e7f56082c0564cf924

--- common.mk
+++ common.mk
@@ -122,8 +122,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5 \
     camera.device@3.2-impl \
     libbson.vendor \
     libxml2 \
