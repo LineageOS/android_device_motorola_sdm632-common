e34bca1f42f621f2d836862114554226b937678c
Andrew Hexen <SyberHexen@gmail.com>
2020-08-01 16:37:01 -0700

    sdm632-common: update vendor SPL from river 'QPU30.52-2-5'
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    
    Change-Id: Ib16db268aa6a61eb06e7a96d872779761e50ad9a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -238,7 +238,7 @@ TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
 BOARD_ROOT_EXTRA_FOLDERS := persist
 
 # Vendor Security Patch Level
-VENDOR_SECURITY_PATCH := 2019-11-01
+VENDOR_SECURITY_PATCH := 2020-03-01
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
