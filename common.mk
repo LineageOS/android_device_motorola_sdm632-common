8b2c50a81bcfec3c2a024a61730f56b9147a5f0a
Andrew Hexen <SyberHexen@gmail.com>
2020-08-08 15:30:27 -0700

    sdm632-common: nuke init.oem.panel.sh
    * No longer needed after successfully inline TP modules for channel, ocean, river to the kernel.
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    
    Change-Id: Icfa577bf9a4437062e0fc09990fd697dae7e58aa

--- common.mk
+++ common.mk
@@ -223,7 +223,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.mmi.usb.sh \
-    init.oem.panel.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sensors.sh \
