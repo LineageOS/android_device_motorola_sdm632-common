38b805301310cb05787a0c3f18cde2d7fb89f0e5
Han Wang <416810799@qq.com>
2020-10-25 12:55:53 +0100

    Revert "sdm632-common: Force enable mobile network settings v2"
     * This prop is not honored anymore on 11.
    
    This reverts commit 9c3436802860786f782335e08dc5ad4c1a009a1d.
    
    Change-Id: I7a10d9918a0218d607d65203e9a38d3e484a618f

--- properties.mk
+++ properties.mk
@@ -176,7 +176,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.radio.aosp_usr_pref_sel=true \
-    persist.sys.fflag.override.settings_network_and_internet_v2=true \
     persist.vendor.cne.feature=1 \
     persist.vendor.dpm.feature=0 \
     DEVICE_PROVISIONED=1 \
