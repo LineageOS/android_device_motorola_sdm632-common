0ee60489bb8f30859dd7ca71aa38defc63ce4ef3
Woody Lin <woodylin@google.com>
2020-10-28 11:22:19 +0800

    sdm660-common: Enable zygote critical window
    Bug: 141948707
    Change-Id: I5dc57312118b0f6d95fc9fe164363ec7e15f1b51

--- properties.mk
+++ properties.mk
@@ -256,3 +256,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=180 \
     ro.zram.periodic_wb_delay_hours=24
+
+# Zygote
+PRODUCT_PROPERTY_OVERRIDES += \
+    zygote.critical_window.minute=10
