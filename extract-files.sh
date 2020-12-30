5d2776dd1de0c9713d91568af82717b8273f0283
Erfan Abdi <erfangplus@gmail.com>
2020-12-30 23:38:25 +0330

    sdm632-common: Fix camera recording
    * Patched libmmcamera2_pproc_modules to not look for manufacturer prop, when
      it finds motorola as manufacturer camera hal will start some processes that
      leads to crash on android R...
    * I'm not sure whats downside of this hexpatch but camera recording is fixed...
    
    Change-Id: I8f67d0b07063df30af325a6b8d4181c90c18202f

--- extract-files.sh
+++ extract-files.sh
@@ -77,6 +77,11 @@ function blob_fixup() {
         patchelf --add-needed libcutils_shim.so "${2}"
         ;;
 
+    # Fix camera recording
+    vendor/lib/libmmcamera2_pproc_modules.so)
+        sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${2}"
+        ;;
+
     # Fix xml version
     product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
         sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
