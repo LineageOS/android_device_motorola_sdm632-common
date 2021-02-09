37b7d6c64f19c2c76085eed3d1b321b8e91b89b9
dianlujitao <dianlujitao@lineageos.org>
2021-02-09 18:14:04 +0800

    sdm632-common: Switch to source built vendor.qti.hardware.perf@2.0.so
    For some reason, any prebuilt ones from pre-R ROMs no longer work on R
    and cause the following error:
      E ANDR-PERF: Unable to link to gPerfHal death notifications!
    
    Change-Id: Ia5de8c300d71b6def223e97ef9d58898c9c11a57

--- common.mk
+++ common.mk
@@ -359,7 +359,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.0.vendor
 
 # QMI
 PRODUCT_PACKAGES += \
