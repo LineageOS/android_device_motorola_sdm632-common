86521c803dd3dbd8f42b73f1fce3def29bdee7eb
dianlujitao <dianlujitao@lineageos.org>
2021-04-26 20:39:23 +0800

    Revert "sdm632-common: Switch to Audio HAL V6"
    Some target devices use pre-R audio.bluetooth.default.so and
    pre-R audio hal, in which the new set_event_callback API is not
    implemented and the audio service will crash due to mismatched struct.
    
    This reverts commit 9b85e7676874763d9ad4920c808f010ffae028ab.
    
    Change-Id: Ia330ac05d84d3dfb3a639458d66a3dc29689e67c

--- common.mk
+++ common.mk
@@ -75,9 +75,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio@5.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.audio.effect@6.0-impl:32 \
+    android.hardware.audio.effect@5.0-impl:32 \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
