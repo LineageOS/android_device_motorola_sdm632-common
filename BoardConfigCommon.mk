cd16b9c119f0a26c4de67440482025ca3cc1afa5
Nolen Johnson <johnsonnolen@gmail.com>
2021-05-03 12:13:04 -0400

    sdm632-common: Update to motorola/river_retail/river:10/QPUS30.52-33-11/df129:user/release-keys
    Change-Id: If50d326e067db5d0848824e2ffb87f9a212010e4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -230,7 +230,7 @@ TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
 BOARD_ROOT_EXTRA_FOLDERS := persist
 
 # Vendor Security Patch Level
-VENDOR_SECURITY_PATCH := 2020-03-01
+VENDOR_SECURITY_PATCH := 2021-02-01
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
