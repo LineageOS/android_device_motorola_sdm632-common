cb3e23daf6f6aae08570f36873a6737e0a2c3130
Deivid21 <david.parra.ignacio@gmail.com>
2021-05-08 00:04:39 -0400

    sdm632-common: Fix unknown symbol when extract blobs
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>
    Change-Id: Ib9eac011efb6e7ba19df050896cfd0540bcc3415

--- extract-files.sh
+++ extract-files.sh
@@ -67,7 +67,7 @@ function blob_fixup() {
         # Fix missing symbols
         vendor/lib64/libril-qc-hal-qmi.so)
             for  LIBRIL_SHIM in $(grep -L "libcutils_shim.so" "${2}"); do
-                $"{PATCHELF}" --add-needed "libcutils_shim.so" "$LIBRIL_SHIM"
+                "${PATCHELF}" --add-needed "libcutils_shim.so" "$LIBRIL_SHIM"
             done
             ;;
         # Fix xml version
@@ -83,7 +83,7 @@ function blob_fixup() {
         # qsap shim
         vendor/lib64/libmdmcutback.so)
             for  LIBQSAP_SHIM in $(grep -L "libqsap_shim.so" "${2}"); do
-                $"{PATCHELF}" --add-needed "libqsap_shim.so" "$LIBQSAP_SHIM"
+                "${PATCHELF}" --add-needed "libqsap_shim.so" "$LIBQSAP_SHIM"
             done
             ;;
     esac
