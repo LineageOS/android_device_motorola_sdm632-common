abefdc4000de6cd286abb56bfb98063a33034181
Steven Moreland <smoreland@google.com>
2021-05-26 00:41:38 +0000

    sdm632-common: Move /vendor/ueventd.rc to /vendor/etc/ueventd.rc
    The root location of this file is deprecated for Android T.
    
    Bug: 189268918
    Bug: 178221726
    Test: device reaches boot complete after raising API level to 32
    Change-Id: I97a6d2e5cacd0ac5eeb81daafeb8b1a0ad5c119b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -119,7 +119,7 @@ LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := root/ueventd.qcom.rc
-# this needs to be in /vendor/ueventd.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
+# this needs to be in /vendor/etc/ueventd.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
