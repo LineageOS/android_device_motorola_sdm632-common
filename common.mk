5e532ccb3684e021ceb003a7afc1c12780ca0cf5
SGCMarkus <markusornik@gmail.com>
2022-02-20 16:57:16 +0100

    sdm632-common: rootdir: remove init.mmi.usb.sh
    Change-Id: I85fcc486e19b59ccf27f4b658538c768c7dc29db

--- common.mk
+++ common.mk
@@ -213,7 +213,6 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_PACKAGES += \
     init.class_main.sh \
-    init.mmi.usb.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sensors.sh \
