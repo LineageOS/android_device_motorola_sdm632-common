ca102eff92b481b2d3ad9d54282134cfd31188f4
Jarl-Penguin <jarlpenguin@outlook.com>
2022-02-21 01:53:44 -0800

    sdm632-common: Switch to FM2
    * Drop unused flags
    * Set up dummy script since we don't need firmware downloading
    * Switch back to vendor.hw.fm.init
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I9bb94233274094fb8b83e8c7835fa9563e8fae1d

--- common.mk
+++ common.mk
@@ -149,8 +149,8 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
+    FM2 \
+    qcom.fmradio
 
 $(call soong_config_set_bool,libfmjni,no_fm_firmware,true)
 
@@ -215,6 +215,7 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.sh \
     init.qcom.sensors.sh \
+    init.qti.fm.sh \
     init.qti.qseecomd.sh
 
 PRODUCT_PACKAGES += \
@@ -223,6 +224,7 @@ PRODUCT_PACKAGES += \
     init.mmi.chipset.rc \
     init.mmi.overlay.rc \
     init.qcom.rc \
+    init.qti.fm.rc \
     init.target.rc \
     ueventd.qcom.rc
 
