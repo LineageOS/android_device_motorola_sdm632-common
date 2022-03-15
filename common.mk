1f401ce4e673e80537272420ec827647b05aeca5
Bruno Martins <bgcngm@gmail.com>
2022-03-15 20:53:47 +0000

    sdm632-common: Copy cgroups and task_profiles directly from system/core
    This ensures that the most up-to-date JSON files are shipped.
    
    Change-Id: Ic9fae188784825ae6dc607c13b403e37290b7067

--- common.mk
+++ common.mk
@@ -97,6 +97,11 @@ PRODUCT_PACKAGES += \
     libbson.vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
