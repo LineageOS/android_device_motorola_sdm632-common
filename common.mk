36ef489cfc78bffce85998c6586c4d2b5f81cb15
LuK1337 <priv.luk@gmail.com>
2022-03-16 01:30:14 +0100

    sdm632-common: Remove trust HAL from product packages
    USB 1.3 HAL now provides the same functionality and even more.
    
    Change-Id: Idda908a90d3b935e1065eb41abec4babeefc4fd0

--- common.mk
+++ common.mk
@@ -427,10 +427,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti \
