c436e73852f2438a21374e6a9e5fdb05564ecf6f
Bruno Martins <bgcngm@gmail.com>
2022-04-05 10:34:38 +0100

    sdm632-common: Inherit from QTI FCM
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -130,6 +130,8 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 LOC_HIDL_VERSION := 3.0
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(PLATFORM_PATH)/framework_manifest.xml
