10c567835fbcd9bfb7438d3cb0e59790572b196e
Danny Trunk <dtrunk90@gmail.com>
2022-09-18 20:10:34 +0200

    sdm632-common: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- common.mk
+++ common.mk
@@ -92,7 +92,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.1-impl:32 \
+    android.hardware.bluetooth.audio-impl:32 \
     libbt-vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
