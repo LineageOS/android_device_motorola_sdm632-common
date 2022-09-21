2b6122425bc6d5b09c37ba9eae54381baec19983
Nolen Johnson <johnsonnolen@gmail.com>
2022-09-21 18:08:50 -0400

    Revert "sdm632-common: Adding GSI keys"
    This reverts commit 5c5f8e792bf393a3cddce8ede9d97673b0620542.
    
    river/ocean/channel have no first-stage ramdisk and there
    is no need to install GSI keys there.
    
    Bug: 140873290 | AOSP
    Test: tree hugger
    
    Change-Id: I4dfe0bb0d88dbbbcaa6742cef9f2405d3b7936d6

--- common.mk
+++ common.mk
@@ -24,9 +24,6 @@
 
 $(call inherit-product, vendor/motorola/sdm632-common/sdm632-common-vendor.mk)
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
