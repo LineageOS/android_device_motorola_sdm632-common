a1eed4abdfb73412c7ef211df1084210a1a479fd
Alexander Martinz <amartinz@shiftphones.com>
2022-10-17 18:16:15 +0200

    Revert "sdm632-common: Disable APEX compression"
    This reverts commit da5affcf2259c98ac1bdc70c2facd3604f4c7e9c.
    
    Compressed APEX support is fixed kernel side.
    
    Test: build, flash and boot device with flag reverted.
    Test: boot GSI with compressed APEX enabled.
    
    Change-Id: I5988614cc055d565b17a5c8d830e35feb0e33e92

--- common.mk
+++ common.mk
@@ -27,10 +27,6 @@ $(call inherit-product, vendor/motorola/sdm632-common/sdm632-common-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Disable APEX compression
-# Keep this after including updatable_apex.mk
-PRODUCT_COMPRESSED_APEX := false
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
