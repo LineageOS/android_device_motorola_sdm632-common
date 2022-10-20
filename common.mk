dbf438978e14b5e1b750a9e8876240a7dfb46d39
Nolen Johnson <johnsonnolen@gmail.com>
2022-10-20 11:32:05 -0400

    sdm632-common: Copy 64-bit variant of libutils-v32
    Change-Id: I3ed5089244cf676205f0c8f33c5a4928ad6efb68

--- common.mk
+++ common.mk
@@ -451,7 +451,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm/arch-arm-armv7-a-neon/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so \
-    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so
 
 # Wifi
 PRODUCT_COPY_FILES += \
