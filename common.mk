8b8cc436342f2ef465608365dcff7677a8665acb
Nolen Johnson <johnsonnolen@gmail.com>
2022-10-20 11:59:53 -0400

    sdm632-common: Enforce EPPE
    Change-Id: I47e434dc3f1251bcd5266d53871aa675268ac2d5

--- common.mk
+++ common.mk
@@ -261,10 +261,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
-# Media / StagefrightCodec 2.0
-PRODUCT_PACKAGES += \
-    libstagefright_ccodec
-
 # MotoActions
 PRODUCT_PACKAGES += \
     MotoActions
