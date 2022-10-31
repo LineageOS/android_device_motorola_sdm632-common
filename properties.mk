3ac8e5130c1297f164890ed77b12a7e4de5f9435
Bruno Martins <bgcngm@gmail.com>
2022-10-31 16:24:31 +0000

    sdm632-common: Set block_binder_thread_on_incoming_calls in product.prop
    Fails to be set from vendor otherwise:
    
      init: Do not have permissions to set 'ro.telephony.block_binder_thread_on_incoming_calls' to 'false' in property file '/vendor/build.prop': SELinux permission check failed
    
    Change-Id: I3c3ea61af240557f3b1053497c3ed1403f7ae318

--- properties.mk
+++ properties.mk
@@ -145,6 +145,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore_desede=true
 
 # Radio
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.telephony.block_binder_thread_on_incoming_calls=false
+
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
@@ -190,7 +193,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.net.doxlat=true \
     persist.vendor.qc.sub.rdump.on=1 \
     persist.vendor.qc.sub.rdump.max=3 \
-    ro.telephony.block_binder_thread_on_incoming_calls=false \
     ro.telephony.call_ring.multiple=false \
     ro.telephony.default_network=10,10 \
     ro.vendor.telephony.default_network=10,10 \
