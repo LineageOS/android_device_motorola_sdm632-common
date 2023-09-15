f0785f30864ae43c774371c258425b664b9a896f
ROMSG <romsg85@gmail.com>
2023-09-14 20:53:30 -0700

    sdm632-common: Add LMKD/PSI props
    Change-Id: Id9e36ca0f31a888214f07d6ae66cde3667526774

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -465,16 +465,6 @@ function configure_memory_parameters() {
     # All targets will use vmpressure range 50-70,
     # All targets will use 512 pages swap size.
     #
-    # Set Low memory killer minfree parameters
-    # 32 bit Non-Go, all memory configurations will use 15K series
-    # 32 bit Go, all memory configurations will use uLMK + Memcg
-    # 64 bit will use Google default LMK series.
-    #
-    # Set ALMK parameters (usually above the highest minfree values)
-    # vmpressure_file_min threshold is always set slightly higher
-    # than LMK minfree's last bin value for all targets. It is calculated as
-    # vmpressure_file_min = (last bin - second last bin ) + last bin
-    #
     # Set allocstall_threshold to 0 for all targets.
     #
 
@@ -491,71 +481,6 @@ else
     arch_type=`uname -m`
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
     MemTotal=${MemTotalStr:16:8}
-
-    # Set parameters for 32-bit Go targets.
-    if [ $MemTotal -le 1048576 ] && [ "$low_ram" == "true" ]; then
-        # Disable KLMK, ALMK, PPR & Core Control for Go devices
-        echo 0 > /sys/module/lowmemorykiller/parameters/enable_lmk
-        echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-        echo 0 > /sys/module/process_reclaim/parameters/enable_process_reclaim
-        disable_core_ctl
-        # Enable oom_reaper for Go devices
-        if [ -f /proc/sys/vm/reap_mem_on_sigkill ]; then
-            echo 1 > /proc/sys/vm/reap_mem_on_sigkill
-        fi
-    else
-
-        # Read adj series and set adj threshold for PPR and ALMK.
-        # This is required since adj values change from framework to framework.
-        adj_series=`cat /sys/module/lowmemorykiller/parameters/adj`
-        adj_1="${adj_series#*,}"
-        set_almk_ppr_adj="${adj_1%%,*}"
-
-        # PPR and ALMK should not act on HOME adj and below.
-        # Normalized ADJ for HOME is 6. Hence multiply by 6
-        # ADJ score represented as INT in LMK params, actual score can be in decimal
-        # Hence add 6 considering a worst case of 0.9 conversion to INT (0.9*6).
-        # For uLMK + Memcg, this will be set as 6 since adj is zero.
-        set_almk_ppr_adj=$(((set_almk_ppr_adj * 6) + 6))
-        echo $set_almk_ppr_adj > /sys/module/lowmemorykiller/parameters/adj_max_shift
-
-        # Calculate vmpressure_file_min as below & set for 64 bit:
-        # vmpressure_file_min = last_lmk_bin + (last_lmk_bin - last_but_one_lmk_bin)
-        minfree_series=`cat /sys/module/lowmemorykiller/parameters/minfree`
-        minfree_1="${minfree_series#*,}" ; rem_minfree_1="${minfree_1%%,*}"
-        minfree_2="${minfree_1#*,}" ; rem_minfree_2="${minfree_2%%,*}"
-        minfree_3="${minfree_2#*,}" ; rem_minfree_3="${minfree_3%%,*}"
-        minfree_4="${minfree_3#*,}" ; rem_minfree_4="${minfree_4%%,*}"
-        minfree_5="${minfree_4#*,}"
-
-        vmpres_file_min=$((minfree_5 + (minfree_5 - rem_minfree_4)))
-        vmpres_file_min_critical=$((vmpres_file_min + (vmpres_file_min - minfree_5)))
-        echo $vmpres_file_min > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
-        echo $vmpres_file_min_critical > /sys/module/lowmemorykiller/parameters/vmpressure_file_min_critical
-
-        if [ "$arch_type" == "aarch64" ]; then
-            echo 1048576 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min_critical
-        else
-            # Disable Core Control, enable KLMK for non-go 8909.
-            if [ "$ProductName" == "msm8909" ]; then
-                disable_core_ctl
-                echo 1 > /sys/module/lowmemorykiller/parameters/enable_lmk
-            fi
-        fi
-
-        # Enable adaptive LMK for all targets &
-        # use Google default LMK series for all 64-bit targets >=2GB.
-        echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-
-        # moto add, Disable adaptive LMK for target > 3GB
-        if [ $MemTotal -gt 3145728 ]; then
-            echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-        fi
-
-        # Enable oom_reaper
-        if [ -f /sys/module/lowmemorykiller/parameters/oom_reaper ]; then
-            echo 1 > /sys/module/lowmemorykiller/parameters/oom_reaper
-        fi
 
         # Set PPR parameters
         if [ -f /sys/devices/soc0/soc_id ]; then
