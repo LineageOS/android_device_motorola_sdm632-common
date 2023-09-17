b57b079e1b99d0ac2307eb07745d4b7a603bc76c
ROMSG <romsg85@gmail.com>
2023-09-16 22:24:57 -0700

    sdm632-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I994ea71cbf24f6022f92e12fa95bbcd9957c7948

--- common.mk
+++ common.mk
@@ -403,6 +403,11 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 QCOM_SOONG_NAMESPACE := $(LOCAL_PATH)/qcom-caf
 
+# Speed Profiles
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
