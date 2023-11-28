63e393e4d0e88f52d953e21ddca023efcd11a1af
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-27 19:37:42 -0500

    sdm632-common: Move ODM props to odm.prop
    Change-Id: I8fff9b7ff44eb15abef455abcd7623d5716d9c85

--- common.mk
+++ common.mk
@@ -336,12 +336,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.va_aosp.support=1
-
-PRODUCT_ODM_PROPERTIES += \
-    ro.vendor.qti.va_odm.support=1
-
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
