f60b55e1c1a7cfc3ea019d93e7b93674620deca8
Nolen Johnson <johnsonnolen@gmail.com>
2023-12-25 22:39:49 -0500

    sdm632-common: Specify androidboot.boot_devices
    So that /dev/block/by-name/ symlinks are created.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I270c1899e4487ed5e7657038c4ed07e407476953

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,7 +145,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_sdm632
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci androidboot.usbconfigfs=true
-BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.boot_devices=soc/7824900.sdhci
 BOARD_KERNEL_CMDLINE += androidboot.veritymode=eio
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
