083c15f4f4e7648168a92c465e7ed74c2443dbc7
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:56:09 +0300

    sdm632-common: Switch to QTI vibrator HAL
    Change-Id: I911001c14167f7dfceb609090f7b90117310da07

--- common.mk
+++ common.mk
@@ -426,8 +426,10 @@ PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 # Update this list with what each blob is actually for
