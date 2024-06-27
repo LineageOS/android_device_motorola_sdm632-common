9963db45b64235400e218f9654c8d52133d5e199
Nolen Johnson <johnsonnolen@gmail.com>
2024-06-27 12:32:14 -0400

    sdm632-common: Build libcurl.vendor
    Change-Id: Ia402afac40c0800370fadf613d324ca61004fa99

--- common.mk
+++ common.mk
@@ -157,6 +157,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0-impl-qti:64 \
     android.hardware.gnss@2.0-service-qti \
     libbatching \
+    libcurl.vendor \
     libgeofencing \
     libgnss \
     libgnsspps \
