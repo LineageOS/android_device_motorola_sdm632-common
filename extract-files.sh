e456c8bde172e93cd0f494490f4fdde79a279bce
Andrew Hexen <SyberHexen@gmail.com>
2024-11-29 07:57:30 -0800

    sdm632-common: decommon device specific blob fixups
    * Fixes the function not patching files correctly when using --only-target
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I4a24b658d97101f9eb68579509fece3aed2cca14

--- extract-files.sh
+++ extract-files.sh
@@ -67,11 +67,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "s/\/product\/framework\//\/system_ext\/framework\//g" "${2}"
             ;;
-        # Fix camera recording
-        vendor/lib/libmmcamera2_pproc_modules.so)
-            [ "$2" = "" ] && return 0
-            sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${2}"
-            ;;
         vendor/etc/data/dsi_config.xml|vendor/etc/data/netmgr_config.xml)
             [ "$2" = "" ] && return 0
             fix_xml "${2}"
@@ -96,13 +91,6 @@ function blob_fixup() {
                 "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
             done
             ;;
-        # memset shim
-        vendor/bin/charge_only_mode)
-            [ "$2" = "" ] && return 0
-            for  LIBMEMSET_SHIM in $(grep -L "libmemset_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libmemset_shim.so" "$LIBMEMSET_SHIM"
-            done
-            ;;
         # Fix missing symbols
         vendor/bin/pm-service)
             [ "$2" = "" ] && return 0
@@ -113,13 +101,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i 's|/system/framework|/vendor/framework|' "${2}"
             ;;
-        # Fix missing symbols
-        vendor/lib/libmot_gpu_mapper.so)
-            [ "$2" = "" ] && return 0
-            for LIBGUI_SHIM in $(grep -L "libgui_shim_vendor.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libgui_shim_vendor.so" "${LIBGUI_SHIM}"
-            done
-            ;;
         # qsap shim
         vendor/lib64/libmdmcutback.so)
             [ "$2" = "" ] && return 0
@@ -128,7 +109,7 @@ function blob_fixup() {
             done
             ;;
         # libutils-v32
-        vendor/lib/soundfx/libspeakerbundle.so | vendor/lib/sensors.rp.so | vendor/lib64/sensors.rp.so)
+        vendor/lib/sensors.rp.so | vendor/lib64/sensors.rp.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
             ;;
