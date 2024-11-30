fa17bafcb9e881de3e4592c22ac4cf41fcbcfefb
Andrew Hexen <SyberHexen@gmail.com>
2024-11-30 15:12:58 -0800

    sdm632-common: display: convert libgrallocutils to .bp
    Change-Id: I75d6ec1b1e5fd6c7220debaea351243418b30056

--- qcom-caf/display/gralloc/Android.mk
+++ qcom-caf/display/gralloc/Android.mk
@@ -35,20 +35,3 @@ ifeq ($(call is-board-platform-in-list, msm8909), true)
 endif
 
 include $(BUILD_SHARED_LIBRARY)
-
-#libgrallocutils
-include $(CLEAR_VARS)
-LOCAL_MODULE                  := libgrallocutils
-LOCAL_VENDOR_MODULE           := true
-LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-ifeq ($(TARGET_KERNEL_VERSION), 4.14)
-LOCAL_C_INCLUDES              += system/core/libion/include \
-                                 system/core/libion/kernel-headers
-endif
-LOCAL_HEADER_LIBRARIES        := display_headers
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libdl android.hardware.graphics.common@1.1
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"grallocutils\" -Wno-sign-conversion
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
-LOCAL_SRC_FILES               := gr_utils.cpp gr_adreno_info.cpp
-include $(BUILD_SHARED_LIBRARY)
