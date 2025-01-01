601fc5f02d67cae80e14532fdf0cf95916cf787a
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 02:03:40 +0200

    display: Remove deprecated lights.$(TARGET_BOARD_PLATFORM)
    AIDL light HAL is required as of U QPR3.
    
    Change-Id: I010fe28125c9532bfa02e5a6e3009f1fd7358d3f

--- qcom-caf/display/Android.mk
+++ qcom-caf/display/Android.mk
@@ -10,10 +10,6 @@ endif
 
 display-hals += gralloc
 
-ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
-display-hals += liblight
-endif
-
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
