8fb964e7e37caf795117deaa5f732bddd7a33728
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 03:50:53 +0200

    sdm632-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3

--- common.mk
+++ common.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     gralloc.msm8953 \
-    hwcomposer.msm8953 \
+    hwcomposer.qcom \
     vendor.qti.hardware.memtrack-service
 
 # DRM
