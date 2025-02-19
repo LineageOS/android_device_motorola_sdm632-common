156771b578c4d8b2890954c904718f8283257354
LuK1337 <priv.luk@gmail.com>
2025-02-19 21:08:26 +0100

    sdm632-common: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- common.mk
+++ common.mk
@@ -203,7 +203,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.msm8953
+    android.hardware.light@2.0-service.lineage
 
 # Media
 PRODUCT_COPY_FILES += \
