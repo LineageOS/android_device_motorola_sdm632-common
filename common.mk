57b0b8730cc197081234512495e45c865c7a1a79
Michael Bestas <mkbestas@lineageos.org>
2025-02-19 16:41:57 +0100

    sdm632-common: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- common.mk
+++ common.mk
@@ -248,6 +248,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     dsp_symlink \
 
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint \
+    vendor_fsg_mountpoint
+
 # Perf
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
