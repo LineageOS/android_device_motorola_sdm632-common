9a846c486edce5b4b2788e030da3e524f992c1e6
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-10 15:36:30 -0400

    sdm632-common: Migrate away from TARGET_RECOVERY_DEVICE_MODULES
    * Dead as of QPR2.
    
    Change-Id: I05bcf246266b89fa7da964f7aa75870b5a63a3b9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,7 +128,6 @@ TARGET_FS_CONFIG_GEN += \
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(PLATFORM_PATH):libinit_sdm632
-TARGET_RECOVERY_DEVICE_MODULES := libinit_sdm632
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1
