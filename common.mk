8cb5a6f2bac61156f1bb8cff11a7a83a87aa7acf
Michael Bestas <mkbestas@lineageos.org>
2025-03-11 02:17:16 +0200

    sdm632-common: Add hardware/motorola to soong namespaces
    Change-Id: I1606d2dc9cef82a4ce9d8764c1f52b59d183e9b2

--- common.mk
+++ common.mk
@@ -387,7 +387,9 @@ PRODUCT_PACKAGES += \
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/motorola
+
 QCOM_SOONG_NAMESPACE := $(LOCAL_PATH)/qcom-caf
 
 # Speed Profiles
