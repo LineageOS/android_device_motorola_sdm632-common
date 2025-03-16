669c689cbbb9c3a44b17d02f87b51de7960ae6b2
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-16 01:44:10 -0400

    sdm632-common: Don't build dependencies & clean makefile
    * soft_omx is dead, libbson isn't used.
    
    Change-Id: I2e84057a38accbb49634b51a50a1e171a620f185

--- common.mk
+++ common.mk
@@ -52,8 +52,7 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     libaudio-resampler \
     libqcomvoiceprocessing \
-    libvolumelistener \
-    libstagefright_softomx_plugin.vendor
+    libvolumelistener
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
@@ -86,11 +85,8 @@ $(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
-    libbson.vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
 # Cgroup and task_profiles
@@ -110,10 +106,8 @@ $(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
 
 # Display
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0.vendor \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.common@1.1.vendor \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     gralloc.msm8953 \
@@ -122,17 +116,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey \
-    libcrypto_shim.vendor \
-    libhidlmemory.vendor
-
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1.vendor
-
-# Gatekeeper
-PRODUCT_PACKAGES += \
-    libion.vendor
+    android.hardware.drm-service.clearkey
 
 # FM
 PRODUCT_PACKAGES += \
@@ -155,7 +139,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0-impl-qti:64 \
     android.hardware.gnss@2.0-service-qti \
     libbatching \
-    libcurl.vendor \
     libgeofencing \
     libgnss \
     libgnsspps \
@@ -234,14 +217,9 @@ PRODUCT_PACKAGES += \
     MotoActions \
     MotoCommonOverlay
 
-# netmgrd
-PRODUCT_PACKAGES += \
-    android.system.net.netd@1.1.vendor
-
-
 # Partitions
 PRODUCT_PACKAGES += \
-    dsp_symlink \
+    dsp_symlink
 
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
@@ -290,11 +268,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
-# Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
-
 # OMX
 PRODUCT_PACKAGES += \
     libOmxCore \
@@ -304,8 +277,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor
+    android.hardware.power-service-qti
 
 # QCOM
 PRODUCT_COPY_FILES += \
@@ -319,8 +291,7 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
-    CarrierConfigOverlay \
-    libprotobuf-cpp-full
+    CarrierConfigOverlay
 
 # Recovery
 TARGET_RECOVERY_DENSITY := xhdpi
@@ -343,17 +314,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
-    android.hardware.sensors@1.0-service \
-    libpower.vendor \
-    libutils-v32
-
-# Shims
-PRODUCT_PACKAGES += \
-    libgui_shim \
-    libgui_shim_vendor \
-    libcutils_shim.vendor \
-    libmemset_shim \
-    libqsap_sdk
+    android.hardware.sensors@1.0-service
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
@@ -381,10 +342,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl:64 \
     android.hardware.thermal@1.0-service
 
-# Touchscreen
-PRODUCT_PACKAGES += \
-    libtinyxml2
-
 # Update Engine
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -411,14 +368,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
-# VNDK
-# Update this list with what each blob is actually for
-# libstdc++: camera.msm8953
-PRODUCT_PACKAGES += \
-    libstdc++_vendor \
-    libgui_vendor:32 \
-    libutils-v33
-
 # Wifi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
@@ -429,10 +378,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
     hostapd_cli \
-    libnl \
-    libqsap_sdk \
     libwifi-hal-qcom \
-    libwpa_client \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
