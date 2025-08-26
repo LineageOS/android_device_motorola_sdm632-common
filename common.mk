d0b312cf368abe5f07ae1c78711fbfb439cf8447
Nolen Johnson <johnsonnolen@gmail.com>
2025-08-26 16:23:06 -0400

    sdm632-common: Don't build sysfs LiveDisplay for now
    * Needs kernel work.
    * Leaving configs in device trees, as I will fix this someday.
    
    Change-Id: I1ed884f46940d6c65f1be72131e6e05725ea61c4

--- common.mk
+++ common.mk
@@ -207,9 +207,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay-service.sysfs
-
 $(call soong_config_set,livedisplay_sysfs,enable_ab,true)
 $(call soong_config_set,livedisplay_sysfs,enable_se,true)
 
